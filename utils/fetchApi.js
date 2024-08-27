fceab35080cdda7a04332b603589de201cc10c1c
AnzalnaKainat <147936573+AnzalnaKainat@users.noreply.github.com>
2024-08-27 21:25:03 +0500

    Update fetchApi.js

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,11 +5,8 @@ export const baseUrl = 'https://bayut.p.rapidapi.com';
 export const fetchApi = async (url) => {
   const { data } = await axios.get((url), {
     headers: {
-      // 'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      // 'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY ,
-      'x-rapidapi-host' : 'bayut.p.rapidapi.com', 
-      'x-rapidapi-key' : 'ea80e6be07msh5bf6a9ef5ace07fp18a6c5jsn692aec443b32',
-
+      'x-rapidapi-host': 'bayut.p.rapidapi.com',
+      'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY ,
     },
   });
     
